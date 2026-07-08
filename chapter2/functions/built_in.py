5a02bf486b83bf6103fbb12158b921fd8288d2bb
Manisha-Tamang <tamangmanisha421@gmail.com>
2026-07-08 16:08:42 +0545

    Add Chapter 2 Python exercies

---
+++ chapter2/functions/built_in.py
@@ -0,0 +1,5 @@
+print("Hello")
+
+length = len("Python")
+print(length)
+
---
+++ chapter2/functions/functions.py
@@ -0,0 +1,30 @@
+# functions is the reusable block of code that executes a specific task only when it is called
+
+# def is the keyword used to define a function
+
+# defination
+# def greet():
+#  print("Hello World!")
+
+# call
+# greet()
+
+# default function
+# def myFun(x, y =50):
+#     print("x: ", x)
+#     print("y: ", y)
+    
+#     myFun(10)
+    
+#     # functions with parameters
+#     def greet(name):
+#         print("Hello,",name)
+        
+#         greet("Alice")
+        
+#         # function with return value
+#         def add(a,b):
+#             return a+b
+        
+#         result = add(10,20)
+#         print(result)
